885511df44e8c7354a9b356f628d3dca1a71ded2
Matias Cardone <zero.xm7@gmail.com>
2021-09-01 17:56:55 -0300

    fixed links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ function Navbar () {
         <div className="container-xl">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" aria-current="page"  to="/">Matias Cardone's website</Link>
+                    <Link className="navbar-brand" aria-current="page"  to="/matscard">Matias Cardone's website</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page"  to="/">Home</Link>
+                        <Link className="nav-link active" aria-current="page"  to="/matscard">Home</Link>
                         </li>
                         <li className="nav-item">
                         <a className="nav-link" href="#profile">About Me</a>
